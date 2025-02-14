4835cba6aeb05f5aff350f688b07a0e2fe6801ab
Mubarak Harran Alketbi <mubarak.harran@gmail.com>
2025-02-14 21:39:52 +0400

    upgraded packages

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# HFDL - Hugging Face Download Manager (version 0.1.10)
+# HFDL - Hugging Face Download Manager (version 0.1.11)
 
 An optimized downloader for Hugging Face models and datasets with intelligent resource management and verification.
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="hfdl",
-    version="0.1.10",
+    version="0.1.11",
     author="Mubarak H. Alketbi",
     author_email="mubarak.harran@gmail.com",
     description="Advanced Hugging Face model/downloader with smart resource management",
@@ -14,13 +14,13 @@ setup(
     url="https://github.com/MubarakHAlketbi/hfdl",
     packages=find_packages(),
     install_requires=[
-        "requests>=2.26.0",
-        "huggingface_hub >=0.19.0",
-        "tqdm>=4.62.0",
-        "portalocker>=2.3.2",
-        "blake3>=0.3.0",
-        "pydantic>=2.0.0",
-        "typing-extensions>=4.0.0"
+        "requests>=2.32.3",
+        "huggingface_hub >=0.28.1",
+        "tqdm>=4.67.1",
+        "portalocker>=3.1.1",
+        "blake3>=1.0.4",
+        "pydantic>=2.10.6",
+        "typing-extensions>=4.12.2"
     ],
     entry_points={
         "console_scripts": [
